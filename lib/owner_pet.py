ea94e18e25be6e25a530ddf3fea429a38fab8a19
lizwachira <lizwanguwachira@gmail.com>
2024-06-12 01:23:55 +0300

    one to many.test pass

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,49 @@
+#two clases `owner``Pet
+# Pet class has properties for the pet's name, type, and owner, and methods to set these properties. 
+# The Owner class has methods to get the owner's pets, add a pet to the owner, and get the owner's pets sorted by name.
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+
+    @pet_type.setter
+    def pet_type(self, pet_type):
+        if pet_type not in self.PET_TYPES:
+            raise Exception('Not a valid pet type.')
+        self._pet_type = pet_type
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("Object is not of type Owner")
+        self._owner = owner
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Input object is not of type Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+    
+    
